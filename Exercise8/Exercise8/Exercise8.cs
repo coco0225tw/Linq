7015d4334e007be48a8c1a0630ec64075cf31d7a
River <coco0958231520@gmail.com>
2023-02-26 20:00:23 +0800

    Update exercise8

---
+++ Exercise8/Exercise8/Exercise8.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise8()
+{
+    string[] cities =
+        { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"};
+    Console.WriteLine("Input starting character for the string : ");
+    var startChar = Console.ReadLine();
+    Console.WriteLine("Input ending character for the string : ");
+    var endChar = Console.ReadLine();
+
+    var ans = from city in cities
+        where city.StartsWith(startChar)
+        where city.EndsWith(endChar)
+        select city;
+
+    if (ans.Any())
+    {
+        Console.WriteLine($"The city starting with {startChar} and ending with {endChar} is : {string.Join(",", ans.ToArray() )}");
+    }
+    else
+    {
+        Console.WriteLine("No corresponded answer");
+
+    }
+}
+Exercise8();
