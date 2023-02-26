b14cdae9c8b2b9218609c3700483d479c8c1e520
River <coco0958231520@gmail.com>
2023-02-27 00:55:38 +0800

    Update exercise23

---
+++ Exercise23/Exercise23/Exercise23.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise23()
+{
+    var chars = new[] { "x", "y", "z" };
+    var nums = new[] { 1, 2, 3, 4 };
+
+    var cartesian = 
+        (from ch in chars
+        from num in nums
+        select new
+        {
+            letterList = ch, numberList = num
+        }).ToList();
+    Console.WriteLine("The cartesian product are :");
+    cartesian.ForEach(Console.WriteLine);
+}
+Exercise23();
