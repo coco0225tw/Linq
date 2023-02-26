f2fd5d1d932b902058285b746f8dae0be3e79b27
River <coco0958231520@gmail.com>
2023-02-27 00:59:56 +0800

    Update exercise24

---
+++ Exercise24/Exercise24/Exercise24.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise24()
+{
+    var chars = new[] { "x", "y" };
+    var nums = new[] { 1, 2, 3 };
+    var colors = new[] { "Green", "Orange"};
+    
+    var cartesian = 
+            (from ch in chars
+            from num in nums
+            from color in colors
+            select new
+            {
+                letterList = ch, numberList = num, colour = color
+            }).ToList();
+    Console.WriteLine("The cartesian product are :");
+    cartesian.ForEach(Console.WriteLine);
+}
+Exercise24();
