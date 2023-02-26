c26037aefaf4e6cb80ac5889b74f0584d4846335
River <coco0958231520@gmail.com>
2023-02-26 19:29:23 +0800

    Update exercise6

---
+++ Exercise6/Exercise6/Exercise6.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+void Exercise6()
+{
+    
+    string[] dayInWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+    foreach (var day in dayInWeek)
+    {
+        Console.WriteLine(day);
+    }
+}
+Exercise6();
---
+++ Exercise6/README.md
@@ -0,0 +1,11 @@
+# Exerxise6
+Write a program in C# Sharp to display the name of the days of a week.
+
+Expected Output:
+Sunday
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday
+Saturday
