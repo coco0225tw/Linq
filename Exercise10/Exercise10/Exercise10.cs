976541c4f149662a847378e051fa30fc40b5a36b
River <coco0958231520@gmail.com>
2023-02-26 22:50:17 +0800

    Update exercise10 without validation and error handler

---
+++ Exercise10/Exercise10/Exercise10.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise10()
+{
+    var i=0;
+    int n,m;
+    List<int> templist = new List<int>(); 
+    
+    Console.WriteLine("Input the number of members on the List : ");  
+    n= Convert.ToInt32(Console.ReadLine());             
+            
+    for(i=0;i<n;i++)
+    {
+        Console.WriteLine("Member {0} : ",i);  
+        var memlist = Convert.ToInt32(Console.ReadLine()); 
+        templist.Add(memlist);
+    }
+            
+    Console.WriteLine("Input the value above you want to display the members of the List : ");  
+    m = Convert.ToInt32(Console.ReadLine());     
+
+    List<int> FilterList = templist.FindAll(x => x > m ? true : false);  
+    Console.WriteLine("The numbers greater than {0} are : ",m);
+    foreach (var num in FilterList)  
+    {  
+        Console.WriteLine(num);  
+    }  
+   
+    Console.ReadLine();  
+}  
+
+
+Exercise10();
---
+++ Exercise10/README.md
@@ -0,0 +1,13 @@
+# Exerxise10
+Write a program in C# Sharp to create a list of numbers and display the numbers greater than 80 as output.
+
+Test Data :
+The members of the list are :
+55 200 740 76 230 482 95
+Expected Output :
+The numbers greater than 80 are :
+200
+740
+230
+482
+95
