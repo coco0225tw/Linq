0cbfc1318f240b91cc07a5e0c466513bf10d04ea
River <coco0958231520@gmail.com>
2023-02-27 01:47:26 +0800

    Update exercise30

---
+++ Exercise30/Exercise30/Exercise30.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise30()
+{
+    List<string> list = new List<string>()
+    {
+        "Biscuit",
+        "Brade",
+        "Butter",
+        "Honey",
+        "Biscuit",
+        "Brade",
+        "Butter",
+        "Honey"
+    };
+
+    list = list.Distinct().ToList();
+
+    list.Sort();
+
+    list.ForEach(Console.WriteLine);
+}
+Exercise30();
