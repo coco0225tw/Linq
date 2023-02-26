056544d93552e8343f39938916d2d61f377d6903
River <coco0958231520@gmail.com>
2023-02-26 23:14:13 +0800

    Update exercise12

---
+++ Exercise12/Exercise12/Exercise12.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+void Exercise12()
+{
+    Console.WriteLine("Input the string");
+    var input = Console.ReadLine();
+    string[] parsedInput = input.Split(new char[] { ' ' });
+    Console.WriteLine("The UPPER CASE words are :");
+
+    foreach (string word in parsedInput)
+    {
+        if (String.CompareOrdinal(word, word.ToUpper()) == 0)
+        {
+            Console.WriteLine(word);
+        } else Console.WriteLine("No corresponded string");
+    }
+}
+Exercise12();
---
+++ Exercise12/README.md
@@ -0,0 +1,9 @@
+# Exerxise12
+Write a program in C# Sharp to find the uppercase words in a string.
+
+Test Data :
+Input the string : this IS a STRING
+Expected Output :
+The UPPER CASE words are :
+IS
+STRING
