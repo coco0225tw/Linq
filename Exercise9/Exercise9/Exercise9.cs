8b66829f2f4f989663bcfe64aef5a83a73dbed4d
River <coco0958231520@gmail.com>
2023-02-26 22:18:27 +0800

    Update exercise9

---
+++ Exercise9/Exercise9/Exercise9.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise9()
+{
+    List<int> numbers = new List<int>() { 55, 200, 740, 76, 230, 482, 95 };
+    var numberGreaterThan80 = numbers.Where(x => x > 80);
+
+    Console.WriteLine("The numbers greater than 80 are :");
+    foreach (var num in numberGreaterThan80)
+    {
+        Console.WriteLine(num);
+    }
+}
+
+Exercise9();
