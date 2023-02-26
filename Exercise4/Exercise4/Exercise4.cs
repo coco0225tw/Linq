ba74529c3063e21d9ed9beeb4883c52bf77f9d96
River <coco0958231520@gmail.com>
2023-02-26 19:14:16 +0800

    Update exercise4

---
+++ Exercise4/Exercise4/Exercise4.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise4()
+{
+    int[] arr = new[] { 5, 5, 5, 4, 6, 7, 9, 9, 5, 4 };
+    
+
+    var ans = from num in arr
+        group num by num into g
+        select new { g.Key, Count = g.Count() };
+
+    foreach (var item in ans)
+    {
+        Console.WriteLine($"Number {item.Key} appears {item.Count} times \n");
+    }
+        
+    
+}
+Exercise4();
---
+++ Exercise4/README.md
@@ -0,0 +1,8 @@
+# Exerxise4
+Write a program in C# Sharp to display the number and frequency of number from giving array.
+
+Expected Output :
+The number and the Frequency are :
+Number 5 appears 3 times
+Number 9 appears 2 times
+Number 1 appears 1 times
