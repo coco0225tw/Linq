bb512e7725fa6ff66c694a86e31e9025abb5da82
River <coco0958231520@gmail.com>
2023-02-26 19:23:33 +0800

    Update exercise5

---
+++ Exercise5/Exercise5/Exercise5.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise5(string[] args)
+{
+    Console.Write("Input the string : ");
+    var str = Console.ReadLine();
+    Console.Write("\n");
+
+    var ans = from word in str
+        group word by word
+        into newWord
+        select newWord;
+    Console.WriteLine("The frequency of the characters are :\n");
+    foreach (var result in ans)
+    {
+        Console.WriteLine($"Character {result.Key}  : {result.Count()} times \n");
+    }
+}
+
+
+Exercise5(new string[] { });
---
+++ Exercise5/README.md
@@ -0,0 +1,11 @@
+# Exerxise5
+Write a program in C# Sharp to display the characters and frequency of character from giving string.
+
+Test Data:
+Input the string: apple
+Expected Output:
+The frequency of the characters are :
+Character a: 1 times
+Character p: 2 times
+Character l: 1 times
+Character e: 1 times
