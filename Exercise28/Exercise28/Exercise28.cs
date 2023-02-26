a19868e09ba9c57296cd3b897fbe6f6bf29d6c06
River <coco0958231520@gmail.com>
2023-02-27 01:33:05 +0800

    Update exercise28

---
+++ Exercise28/Exercise28/Exercise28.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise28()
+{
+    string[] cities =  
+    {  
+        "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"  
+    };  
+    
+    Console.WriteLine("Here is the arranged list :");
+    IEnumerable<string> cityOrder =
+        cities
+            .OrderBy(str => str.Length)
+            .ThenBy(str => str);
+    
+    foreach (string city in cityOrder)
+        Console.WriteLine(city);
+}
+
+Exercise28();
