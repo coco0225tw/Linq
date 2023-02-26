573c350579c65448a5799691d44d5e5410d47818
River <coco0958231520@gmail.com>
2023-02-26 19:39:58 +0800

    Update exercise7

---
+++ Exercise7/Exercise7/Exercise7.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise7()
+{
+    int[] arr = new[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
+
+    var freq = from int num in arr
+        group num by num
+        into newGroup
+        select newGroup ;
+    foreach (var ans in freq)
+    {
+        Console.WriteLine($"{ans.Key} {ans.Key * ans.Count()} {ans.Count()} ");
+    }
+}
+Exercise7();
