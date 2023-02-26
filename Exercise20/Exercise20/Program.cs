d30c3022dd14b26a8076b30886c03b51359e8dab
River <coco0958231520@gmail.com>
2023-02-27 00:20:19 +0800

    Update exercise20

---
+++ Exercise20/Exercise20/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Exercise20()
+{
+    List<string> listOfString = new List<string>()
+    {
+        "m","n","o","p","q"
+    };  
+    
+    foreach(var tchar in listOfString)  
+    {  
+        Console.WriteLine("Char: {0} ", tchar);  
+    } 
+
+    listOfString.RemoveAt(3); 
+    
+    Console.WriteLine("Here is the list after removing item index 3 from the list :");
+    foreach(var rChar in listOfString)  
+    {  
+        Console.WriteLine("Char: {0} ", rChar);  
+    }  
+
+}
+Exercise20();
