44cf4dd3499f558a79fc15b6369639b6d5d1d60e
River <coco0958231520@gmail.com>
2023-02-27 00:07:44 +0800

    Update exercise17

---
+++ Exercise17/Exercise17/Exercise17.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise17()
+{
+    List<char> chars = new List<char>()
+    {
+        'm',
+        'n',
+        'o',
+        'p',
+        'q'
+    };
+    foreach(var tchar in chars)  
+    {  
+        Console.WriteLine("Char: {0} ", tchar);  
+    } 
+    Console.WriteLine("Here is the list after removing the item 'o' from the list : ");
+    chars.Remove('o');
+    foreach(var tchar in chars)  
+    {  
+        Console.WriteLine("Char: {0} ", tchar);  
+    } 
+}
+Exercise17();
