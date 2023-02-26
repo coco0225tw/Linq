2a27da5db12c433348798d990996f3f2586f41c3
River <coco0958231520@gmail.com>
2023-02-27 00:17:23 +0800

    Update exercise19

---
+++ Exercise19/Exercise19/Exercise19.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise19()
+{
+    List<string> listOfString = new List<string>()
+    {
+        "m","n","o","p","q"
+    };  
+    
+    foreach(var tchar in listOfString)  
+    {  
+        Console.WriteLine("Char: {0} ", tchar);  
+    } 
+
+    listOfString.RemoveAll(en => en == "q"); 
+    
+    Console.WriteLine("Here is the list after removing item 'q' from the list : ");
+    foreach(var rChar in listOfString)  
+    {  
+        Console.WriteLine("Char: {0} ", rChar);  
+    }  
+  
+
+}
+Exercise19();
---
+++ Exercise19/README.md
@@ -0,0 +1,17 @@
+# Exerxise19
+Write a program in C# Sharp to remove items from list by passing filters.
+
+Test Data :
+Here is the list of items :
+Char: m
+Char: n
+Char: o
+Char: p
+Char: q
+Expected Output :
+Here is the list after removing item 'q' from the list :
+Char: m
+Char: n
+Char: o
+Char: p
+
