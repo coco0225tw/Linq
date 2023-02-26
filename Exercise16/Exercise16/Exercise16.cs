8d92a42c5d0f48c9c7bd0439afd27752bcb9b30b
River <coco0958231520@gmail.com>
2023-02-27 00:01:15 +0800

    Update exercise16

---
+++ Exercise16/Exercise16/Exercise16.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise16()
+{
+    string[] folderFiles = Directory.GetFiles("D:/mine/Linq/Exercise16");
+    
+		
+    var size = folderFiles.Select(file =>new FileInfo(file).Length).Average();
+    size = Math.Round(size / 10, 1);
+    Console.WriteLine($"The Average file size is {size} MB");
+}
+Exercise16();
---
+++ Exercise16/README.md
@@ -0,0 +1,5 @@
+# Exerxise16
+Write a program in C# Sharp to calculate size of file using LINQ.
+
+Expected Output :
+The Average file size is 3.4 MB
