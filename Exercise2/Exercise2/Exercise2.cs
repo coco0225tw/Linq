b45a52596baa257c7ced4ed32310dceee3837f97
River <coco0958231520@gmail.com>
2023-02-26 18:39:06 +0800

    Update exercise2

---
+++ Exercise2/Exercise2/Exercise2.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+void Exercise2()
+{
+    int[] arr = { 0, 3, 25, 4, -3, 6, 50, 100, 101, 11 };
+
+        var numBetween1to11 =
+            from t in arr
+            where t < 12
+            where t > 0
+            select t;
+
+        foreach (var num in numBetween1to11)
+        {
+            Console.WriteLine(num);
+        }
+        Console.WriteLine("\n\n");
+
+}
+Exercise2();
