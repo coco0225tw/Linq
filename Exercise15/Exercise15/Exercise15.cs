1bd95ac86eac9b06cbc0b4bec82b42aeae64ccfa
River <coco0958231520@gmail.com>
2023-02-26 23:54:45 +0800

    Update exercise15

---
+++ Exercise15/Exercise15/Exercise15.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise15()
+{
+    var files = new List<string>() {
+        "aaa.frx",
+        "bbb.TXT",
+        "xyz.dbf",
+        "abc.pdf",
+        "aaaa.PDF",
+        "xyz.frt",
+        "abc.xml",
+        "ccc.txt",
+        "zzz.txt"};
+    var extensions = files.Select(s =>
+        {
+            var split = s.Split(new char[] { '.' });
+            return split[split.Length - 1].ToUpper();
+        }
+    ).ToList();
+
+    var groupExtensions = from file in extensions
+        group file by file
+        into f
+        select new{ file = f.Key, count = f.Count()
+    }
+    ;
+    
+    groupExtensions.ToList().ForEach(ext =>
+        Console.WriteLine($"number of extension {ext.file} is {ext.count}")
+    );
+}
+Exercise15();
