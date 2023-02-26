d944a752d7fc99d86d02ff8b4b011d1de32dd6bd
River <coco0958231520@gmail.com>
2023-02-27 01:28:04 +0800

    Update exercise27

---
+++ Exercise27/Exercise27/Exercise27.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// See https://aka.ms/new-console-template for more information
+
+void Exercise27()
+{
+    List<TItem> itemList = new List<TItem>
+    {  
+        new TItem { ItemId = 1, ItemDes = "Biscuit  " }, 
+        new TItem { ItemId = 2, ItemDes = "Chocolate" }, 
+        new TItem { ItemId = 3, ItemDes = "Butter   " },  
+        new TItem { ItemId = 4, ItemDes = "Brade    " },  
+        new TItem { ItemId = 5, ItemDes = "Honey    " }  
+    }; 
+		  
+    List<TPurchase> purchaselist = new List<TPurchase>
+    {  
+        new TPurchase { No=100, ItemId = 3,  Qty = 800 }, 
+        new TPurchase { No=101, ItemId = 2,  Qty = 650 }, 
+        new TPurchase { No=102, ItemId = 3,  Qty = 900 },  
+        new TPurchase { No=103, ItemId = 4,  Qty = 700 },
+        new TPurchase { No=104, ItemId = 3,  Qty = 900 },  
+        new TPurchase { No=105, ItemId = 4,  Qty = 650 },  		   
+        new TPurchase { No=106, ItemId = 1,  Qty = 458 }  
+    }; 
+        
+			
+    foreach (var item in itemList)
+    {Console.WriteLine(
+        $"Item Id: {item.ItemId}, Description: {item.ItemDes}"); }
+
+    Console.WriteLine("Here is the Purchase List : ");
+
+    foreach (var item in purchaselist)
+    {
+        Console.WriteLine(
+            $"Invoice No: {item.No}, Item Id : {item.ItemId},  Quantity : {item.Qty}");
+    }
+
+    Console.WriteLine("Here is the list after joining  : ");
+ 
+    var join = 
+        from d in purchaselist 
+        join e in itemList
+            on d.ItemId equals e.ItemId 
+            into a 
+            from b in a.DefaultIfEmpty(new TItem())
+
+            select new  
+            {  
+                itid = b.ItemId,  
+                itdes = b.ItemDes,  
+                prqty = d.Qty  
+            };  
+    Console.WriteLine("Item ID\t\tItem Name\tPurchase Quantity");
+    Console.WriteLine("-------------------------------------------------------");
+    foreach (var data in join)  
+    {  
+        Console.WriteLine(data.itid + "\t\t" + data.itdes + "\t\t" + data.prqty);  
+    }  
+}
+
+Exercise27();
+public class TItem
+{
+    public int ItemId { get; set; }
+    public string ItemDes { get; set; }
+}
+
+public class TPurchase
+{
+    public int No { get; set; }
+    public int ItemId { get; set; }
+    public int Qty { get; set; }
+}
