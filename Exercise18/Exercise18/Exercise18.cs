23badeff91ecbb5abe101cedfc981c21797bda21
River <coco0958231520@gmail.com>
2023-02-27 00:12:42 +0800

    Update exercise18

---
+++ Exercise18/Exercise18/Exercise18.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Exercise18()
+{
+    List<string> listOfString = new List<string>()
+    {
+        "m","n","o","p","q"
+    };  
+    
+    foreach(var tchar in listOfString)  
+    {  
+        Console.WriteLine("Char: {0} ", tchar);  
+    } 
+ 
+    listOfString.Remove(listOfString.FirstOrDefault(en => en == "p")); 
+           
+    
+    Console.WriteLine("Here is the list after removing the item 'p' from the list : ");
+    foreach(var rChar in listOfString)  
+    {  
+        Console.WriteLine("Char: {0} ", rChar);  
+    }
+}
+Exercise18();
