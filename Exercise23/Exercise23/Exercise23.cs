4d7afbc90ddabb8c580ae19aca4c020c126c5ec6
River <coco0958231520@gmail.com>
2023-04-01 17:06:59 +0800

    update e23

--- Exercise23/Exercise23/Exercise23.cs
+++ Exercise23/Exercise23/Exercise23.cs
@@ -4,15 +4,10 @@ void Exercise23()
 {
     var chars = new[] { "x", "y", "z" };
     var nums = new[] { 1, 2, 3, 4 };
-
-    var cartesian =
-        (from ch in chars
-        from num in nums
-        select new
-        {
-            letterList = ch, numberList = num
-        }).ToList();
+    //cross join/Cartesian join => selectMany(from multiple collection)
+    var cartesian = 
+        chars.SelectMany(ch => nums, (ch, num) => new { letterList = ch, numberList = num });
     Console.WriteLine("The cartesian product are :");
-    cartesian.ForEach(Console.WriteLine);
+    cartesian.ToList().ForEach(Console.WriteLine);
 }
 Exercise23();
