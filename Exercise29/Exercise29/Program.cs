d84e1eb33bf3653aa89ab6329d818fceefcf55fa
River <coco0958231520@gmail.com>
2023-04-01 19:11:18 +0800

    update e29

---
+++ Exercise29/Exercise29/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+void Exercise29()
+{
+    var cities =  new string[]
+    {  
+        "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"  
+    };
+    var cityGroup =    cities.Select((x, i) => new { Index = i, Value = x })
+        .GroupBy(x => x.Index / 3)
+        .Select(x => x.Select(v => v.Value).ToArray())
+        ;
+    foreach (var city in cityGroup)
+    {
+        Console.WriteLine(string.Join(";", city));
+        Console.WriteLine("-- here is a group of cities --\n");
+    }
+
+    
+}
+
+Exercise29();
